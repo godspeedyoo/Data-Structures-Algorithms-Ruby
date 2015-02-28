917d11d4b6d51d1fd18cc693bc546287cda9bd2d
anthony-ng <thomsonng123@gmail.com>
2015-02-27 16:29:25 -0800

    added lonely integer question

--- hackerrank.rb
+++ hackerrank.rb
@@ -1,18 +1,27 @@
-# Problem Statement
+# Lonely Integer
 
+# Problem Statement
 # There are N integers in an array A. All but one integer occur in pairs. Your task is to find out the number that occurs only once.
 
 # Input Format
-
 # The first line of the input contains an integer N indicating the number of integers.
 # The next line contains N space separated integers that form the array A.
 
 # Constraints
-
 # 1 <= N < 100
 # N % 2 = 1 ( N is an odd number )
 # 0 <= A[i] <= 100, ∀ i ∈ [1, N]
 
 # Output Format
-
 # Output S, the number that occurs only once.
+
+# Input(stdin)
+# 5
+# 0 0 1 2 1
+
+# Output(stdout)
+# 2
+
+input = 5\n 0 0 1 2 1
+
+print input
